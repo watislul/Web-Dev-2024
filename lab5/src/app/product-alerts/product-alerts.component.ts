871a1110e49a1033b7d0e2842e1675b1394a94de
watislul <123138150+watislul@users.noreply.github.com>
2024-02-28 19:01:37 +0600

    Add files via upload

---
+++ lab5/src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  standalone: false,
+  templateUrl: './product-alerts.component.html',
+  styleUrl: './product-alerts.component.css',
+})
+export class ProductAlertsComponent {
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
+}
